f76056f48e72efb86d89f2d2c74c51ddf7264d35
Ana Karla <anakarlad.21@gmail.com>
2024-05-13 20:52:55 -0300

    get and delete function

--- CRUD/repositories/registerRepository.ts
+++ CRUD/repositories/registerRepository.ts
@@ -11,6 +11,26 @@ class registerRepository {
         const {rows} = await db.query<Register>(query);
         return rows || [];
     }
+
+    async createRegister(data: Register): Promise<void> {
+        const { username, problem, address, reference_point } = data;  
+        const values = [username, problem, address, reference_point];      
+        const query = `
+        INSERT INTO registers (username, problem, address, reference_point)
+        VALUES ($1, $2, $3, $4)
+        `;        
+        
+        await db.query<Register>(query, values);
+    }
+
+    async deleteRegister(uuid: string): Promise<void> {      
+        const query = `
+        DELETE FROM registers
+        WHERE uuid = $1
+        `;        
+
+        await db.query<Register>(query, [uuid]);
+    }
 }
 
 export default new registerRepository();
--- CRUD/src/Routes/routes.ts
+++ CRUD/src/Routes/routes.ts
@@ -4,7 +4,13 @@ import registerRepository from "../../repositories/registerRepository";
 const routes = Router();
 
 routes.post('/register', async (req: Request, res: Response) => {
-
+    try {
+        const { username, problem, address, reference_point } = req.body;
+        await registerRepository.createRegister({ username, problem, address, reference_point });
+        res.status(200).json({ Comment: 'Registro criado!' });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao criar um novo registro' });
+    }
 });
 
 routes.get('/register', async (req: Request, res: Response) => {
@@ -17,7 +23,13 @@ routes.put('/register/:uuid', async (req: Request<{uuid: string}>, res: Response
 });
 
 routes.delete('/register/:uuid', async (req: Request<{uuid: string}>, res: Response) => {
-
+    try {
+        const { uuid } = req.params;
+        await registerRepository.deleteRegister(uuid);
+        res.status(200).json({ Comment: 'Registro deletado!' });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao deletar registro' });
+    }
 });
 
 export default routes;
